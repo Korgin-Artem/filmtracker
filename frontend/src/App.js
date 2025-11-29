69a66d84ff2316004fc43c00562317029067d20a
Korgin-Artem <korgin7704@mail.ru>
2025-11-29 18:51:12 +0300

    Обновление главного App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { StyledEngineProvider } from '@mui/material/styles';
+import theme from './app/config/theme';
+import globalStyles from './app/config/globalStyles';
+import AppRouter from './app/router/AppRouter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {globalStyles}
+        <AppRouter />
+      </ThemeProvider>
+    </StyledEngineProvider>
   );
 }
 
